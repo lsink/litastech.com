412d3da5ca22d708f8f07b5ecaf77ea9e018887f
Larry Sinkevicius <larbear90@gmail.com>
2013-12-18 20:23:25 -0500

    added all files

---
+++ lt/lt/settings/local.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'litastech',
+        'USER': 'user',
+        'PASSWORD': 'user_pass',
+        'HOST': 'localhost',
+    }
+}
