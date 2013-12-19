48d23415bf007a8523cb9f7cab5575a9ef27419a
Larry Sinkevicius <larbear90@gmail.com>
2013-12-19 17:45:50 -0500

    added site_url to projects model

--- lt/projects/models.py
+++ lt/projects/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Projects(models.Model):
 	name = models.CharField(max_length=250)
 	image = models.ImageField(upload_to='projects')
+	site_url = models.CharField(max_length=250, null=True, blank=True)
 	github_url = models.CharField(max_length=250, null=True, blank=True)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+	def __unicode__(self):
+		return self.name
