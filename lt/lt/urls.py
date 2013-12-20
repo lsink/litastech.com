354b16fa9703a57e1f4384823c1da14845f278b7
Larry Sinkevicius <larbear90@gmail.com>
2013-12-19 19:18:52 -0500

    added templates

--- lt/lt/urls.py
+++ lt/lt/urls.py
@@ -6,9 +6,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     # Examples:
     # url(r'^$', 'lt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^$', 'projects.views.home'),
+) 
--- lt/projects/views.py
+++ lt/projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Projects
+
 # Create your views here.
+
+def home(request):
+	all_projects = Projects.objects.all()
+	return render(request, 'home.html', {'projects': all_projects})
